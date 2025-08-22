683e5312a996c102afc2201582896f525fd9c8af
mattyg3 <surfff4life@gmail.com>
2025-08-21 18:48:50 -0700

    added get_player_metadata() to scraper_utils

--- scraper_utils.py
+++ scraper_utils.py
@@ -2,49 +2,138 @@
 import pandas as pd
 import numpy as np
 import bs4
-import enum
-import requests
-import string
+import re
 import time
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 
-##selenium options
-options = Options()
-options.add_argument("--headless")
+
+import requests
+
+
+#### Pro Football Reference scraping utility functions
+
+# def html_JS_scrape(url):
+#     from requests_html import HTMLSession
+#     session = HTMLSession()
+#     r = session.get(url)
+
+#     # Render JavaScript (uses pyppeteer under the hood)
+#     r.html.render(timeout=20)
+
+#     # Extract text or links
+#     print(r.html.find("title", first=True).text)
 
 
 
-#######Pull all Player Page Links into player_links[]
-# Create list of captial letters to loop over
-# letters = list(string.ascii_uppercase)
-letters = ['A', 'C']
+# def playwright_scrape(url):
+#     from playwright.sync_api import sync_playwright
 
-def get_player_hrefs(scrape_url):
+#     with sync_playwright() as p:
+#         browser = p.firefox.launch(headless=True)
+#         page = browser.new_page()
+#         page.goto(url, wait_until="networkidle", timeout=0)
+#         print("Title:", page.title())
+        
+#         # # Extract content
+#         # content = page.content()
+#         # print(content)
+        
+#         browser.close()
+
+# def request_func(url):
+#     headers = {"User-Agent": "Mozilla/5.0"}  
+#     response = requests.get(url, headers=headers)
+#     # soup = bs4.BeautifulSoup(response.text, "lxml")
+#     soup = bs4.BeautifulSoup(response.text, 'html.parser')
+#     print(soup)
+
+def get_html_w_selenium(url):
+
+    #selenium options
+    options = Options()
+    ##Disable images, CSS, and other unnecessary content
+    options.set_preference("permissions.default.image", 2)   # Block images
+    options.set_preference("permissions.default.stylesheet", 2)  # Block CSS
+    options.set_preference("dom.ipc.plugins.enabled.libflashplayer.so", "false")  # Disable Flash
+
+    options.add_argument("--headless")
+    options.add_argument("--width=1920")
+    options.add_argument("--height=1080")
+
+    #Selenium to open, grab html, then close
     driver = webdriver.Firefox(options=options)
-    driver.get(scrape_url)
+    driver.get(url)
     time.sleep(1)
     html_raw = driver.page_source
     driver.close()
+    
+    return html_raw
 
-    soup = bs4.BeautifulSoup(html_raw, 'html.parser')
-    soup_p = soup.find("div", {"id": "div_players"})
 
-    player_links = []
-    for a in soup_p.findAll('a', href=True):
-        s = a['href']
-        # player_links.append(s[11:])
-        player_links.append(s)
+def get_player_hrefs(letters):
+    all_player_links=[]
 
-    return player_links
+    for alpha in letters:
+        url_s =  'https://www.pro-football-reference.com/players/' + alpha + '/'
 
-all_player_links=[]
-for alpha in letters:
-    url_s = 'https://www.pro-football-reference.com/players/' + alpha + '/'
+        html_raw = get_html_w_selenium(url_s)
 
-    all_player_links.append(get_player_hrefs(url_s))
+        ##Parse html with bs4, grab player hrefs
+        soup = bs4.BeautifulSoup(html_raw, 'html.parser')
+        soup_p = soup.find("div", {"id": "div_players"})
+        for a in soup_p.findAll('a', href=True):
+            s = a['href']
+            # all_player_links.append(s[:-4])
+            all_player_links.append(re.sub('.htm', '', s))
+    return all_player_links
 
-print(all_player_links)
+
+
+def get_player_metadata(soup):
+        soup_p = soup.find("div", {"id": "meta"})
+
+        try:
+            player_name = soup_p.find("h1").text.strip()
+        except NameError as e:
+            print("Caught an error:", e)
+            raise ValueError("No player name found (bs4)")  from e
+
+        try:
+            player_position = soup_p.find("strong", string="Position").next_sibling.strip(": ").strip()
+        except NameError as e:
+            print("Caught an error:", e)
+            raise ValueError("No player position found (bs4)")  from e
+
+        try:
+            player_college = soup_p.find("strong", string="College").find_next("a").text.strip()
+        except:
+            None
+
+        return {'player_name': player_name, 'player_position': player_position, 'player_college': player_college}
+
+
+def grab_gamelogs(href_list):
+    for href in href_list:
+        url_s = 'https://www.pro-football-reference.com' + href + '/gamelog/'
+
+        html_raw = get_html_w_selenium(url_s)
+        soup = bs4.BeautifulSoup(html_raw, 'html.parser')
+
+        meta_info = get_player_metadata(soup)
+        
+
+
+
+
+
+
+
+
+
+
+
+#########OLD
 
 
 #######Pull all Player Page Links into player_links[]
